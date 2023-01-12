0d2cb97f9c171236342daccc2e97b04b949be600
Sameer-Ahmed1 <sameernadeemi1111@gmail.com>
2023-01-12 23:46:14 +0530

    Add cell class

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+class Cell {
+  constructor(row, col, parentContainer) {
+    this.location = `${row}${col}`;
+    this.domReference = document.createElement("input");
+    this.domReference.setAttribute("id", this.location);
+    this.domReference.classList.add("grid-item");
+    parentContainer.appendChild(this.domReference);
+  }
+  setFormula(formula) {
+    //extract formula
+  }
+  computeValue() {
+    //compute value
+    this.domReference.setAttribute("value", value);
+  }
+}
+let cells = [];
 function createGrid(n) {
-    let container = document.createElement('div');
-    container.classList.add('grid-container');
-    for (let i = 0; i < 10*n; i++) {
-        let grid = document.createElement('input');
-        grid.type = 'text'
-        grid.classList.add("grid-item")   
-        container.appendChild(grid);
-        grid.setAttribute("row", Math.ceil(i/10));
-        grid.setAttribute("colomn",i%10);
-    }
-    document.body.appendChild(container);
+  let container = document.createElement("div");
+  container.classList.add("grid-container");
+  for (let i = 0; i < 10 * n; i++) {
+    cells[i] = new Cell(Math.ceil(i / 10), i % 10, container);
+  }
+  document.body.appendChild(container);
 }
 createGrid(20);
