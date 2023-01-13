b5adceaa7e9c0bf06777f580dcd95e7b14f1bff4
Sameer-Ahmed1 <97391681+Sameer-Ahmed1@users.noreply.github.com>
2023-01-13 09:49:59 +0530

    Add row and column properties

--- Cell.js
+++ Cell.js
@@ -1,13 +1,17 @@
 export class Cell {
     constructor(row, col, parentContainer) {
-      this.location = `${row}${col}`;
+      this.row = row;
+      this.column = col;
       this.domReference = document.createElement("input");
-      this.domReference.setAttribute("id", this.location);
+      this.domReference.setAttribute("id", `${row}${col}`);
       this.domReference.classList.add("grid-item");
       parentContainer.appendChild(this.domReference);
     }
     setFormula(formula) {
       //extract formula
+    }
+    getDomReference() {
+      return this.domReference;
     }
     getId() {
         return this.domReference.id;
@@ -19,4 +23,5 @@ export class Cell {
     computeFormula() {
       //compute value
     }
+    
   }
