687f37547014ba80e580c7a9b5d2e4ec18702aee
Sameer-Ahmed1 <sameernadeemi1111@gmail.com>
2023-01-15 20:41:36 +0530

    Change input element to div and add content editable functionality

--- Cell.js
+++ Cell.js
@@ -1,16 +1,16 @@
 export class Cell {
-  constructor(row, column,i, parentContainer) {
+  constructor(row, column, parentContainer) {
     this.row = row;
     this.column = column;
-    this.i=i;
-    this.domReference = document.createElement("input");
-    this.domReference.setAttribute("id", `${row}+${column}`);
+    this.domReference = document.createElement("div");
+    this.domReference.setAttribute("id", `${row}${column}`);
     this.domReference.setAttribute("class", "grid-item cell");
     this.domReference.classList.add("grid-item");
     parentContainer.appendChild(this.domReference);
   }
   setFormula(formula) {
     //extract formula
+    this.formula = formula;
   }
   getDomReference() {
     return this.domReference;
@@ -19,7 +19,7 @@ export class Cell {
     return this.domReference.id;
   }
   getValue() {
-    return this.domReference.value;
+    return +this.domReference.textContent;
   }
   computeFormula() {
     //compute value
@@ -36,7 +36,22 @@ export class Cell {
   getColumn() {
     return this.column;
   }
-  setValue(value) {
-    this.value = value;
+  setValue() {
+    this.value = this.domReference.textContent;
+  }
+  toggleFocus() {
+    if (this.isFocused()) {
+      this.domReference.contenteditable = "false";
+      this.domReference.blur();
+    } else {
+      this.domReference.contentEditable = "true";
+      this.domReference.focus();
+      /* https://stackoverflow.com/a/55811159/19767708 */
+      document.execCommand("selectAll", false, null); //execCommand is deprecated but still supported
+      document.getSelection().collapseToEnd();
+    }
+  }
+  isFocused() {
+    return this.domReference === document.activeElement;
   }
 }
