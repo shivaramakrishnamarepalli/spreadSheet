79dc79c9c7e15755378b74e8610f401624f1ba61
Sameer-Ahmed1 <sameernadeemi1111@gmail.com>
2023-01-15 20:42:28 +0530

    Change some navigation function and add formula input

--- index.js
+++ index.js
@@ -1,119 +1,62 @@
-import { Cell } from "./Cell.js";
-let cells = [];
-
+import { createGrid } from "./Grid.js";
+import { isArrowKey } from "./Navigate.js ";
+import { navigate } from "./Navigate.js";
 import { compute } from "./Compute.js";
-console.log(compute("23+44-33*2+6/2"))
 
-function createGrid(n, m) {
-  let container = document.createElement("div");
-  container.classList.add("grid-container");
-  container.style.width = `${156 * m}px`;
-  container.style.height = `${n * 30}px`;
-  container.style.backgroundColor = "rgb(135, 190, 237)";
-
-  for (let i = 1; i <= m * n; i++) {
-    let grid;
-    if (i % m === 1) {
-      grid = document.createElement("div");
-      grid.style.width = "30px";
-      grid.textContent = `${Math.ceil(i / m) - 1}`;
-    } else if (i <= m) {
-      grid = document.createElement("div");
-      grid.textContent = `${
-        Math.ceil(i % m) - 1 === -1 ? m - 1 : Math.ceil(i % m) - 1
-      }`;
-      grid.style.width = "150px";
-    } else {
-      let col =(i % m) - 1;
-      if(col===-1){
-        col=m-1
-      }
-      const cell = new Cell(Math.ceil(i / m) - 1,col ,i, container);
-      cells.push(cell);
-      cell.setWidth("150px");
-    }
-    if (i === 1) {
-      grid.textContent = "";
-    }
-    if (i % m === 1 || i <= m) {
-      grid.type = "text";
-      grid.classList.add("grid-item");
-      container.appendChild(grid);
-      grid.setAttribute("row", Math.ceil(i / m) - 1);
-      grid.setAttribute("colomn", (i % m) - 1);
-    }
-  }
-
-  document.body.appendChild(container);
-}
-createGrid(200, 20); //creating a grid with 20 rows
-
-
-let container = document.querySelector(".grid-container");
-container.addEventListener("click", handleClick);
+let container = document.createElement("div");
+let cells = createGrid(container, 20, 20);
 let current = cells[0];
-current.getDomReference().classList.add("current-cell");
-current.getDomReference().focus()
+current.getDomReference().style.backgroundColor = "red";
+const formulaBox = document.getElementById("formula-box");
+const formulaInputBox = document.getElementById("formula-input");
 
-function handleClick(event) {
+formulaBox.addEventListener("click", handleClickOnFormulaBox);
+container.addEventListener("click", handleClickOnContainer);
+document.addEventListener("keydown", handleKeydown);
+
+function handleClickOnFormulaBox(e) {
+  if (e.target.id !== "formula-enter") return;
+  current.setFormula(formulaInputBox.value);
+}
+function handleClickOnContainer(event) {
   if (event.target.className !== "grid-item cell") return;
-  setCurrent(cells.find((a) => a.getId() == event.target.id));
+  current.getDomReference().style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+  current = cells.find((a) => a.getId() == event.target.id);
+  current.getDomReference().style.backgroundColor = "red";
 }
 
-
-document.onkeydown = checkKey;
-function checkKey(e) {
-
-  if(current.value===undefined)
-  {
-    current.value=""
+function handleKeydown(e) {
+  if (document.activeElement === formulaInputBox) {
+    if (e.keyCode == 13) {
+      current.setFormula(formulaInputBox.value);
+      formulaInputBox.blur();
+    }
+    return;
   }
-  if(!isNaN(e.key))
-  {
-    current.setValue(current.value+e.key);
-  }
-
-  let currentRow = current.row;
-  let currentCol = current.column;
-  if (e.keyCode == '40') {
-    // down arrow
-    let nextRow = currentRow + 1;
-    let nextCell = cells.find((a) => a.row == nextRow && a.column == currentCol);
-    if(nextCell){
-      setCurrent(nextCell)
+  if (isArrowKey(e.keyCode)) {
+    if (!current.isFocused()) {
+      navigate(e, current, setCurrent, cells);
+      return;
     }
   }
-  else if (e.keyCode == '38') {
-    // up arrow
-    let nextRow = currentRow - 1;
-    let nextCell = cells.find((a) => a.row == nextRow && a.column == currentCol);
-    if(nextCell){
-      setCurrent(nextCell)
+  if (e.keyCode == 13) {
+    //'Enter key'
+    /* https://stackoverflow.com/a/61237402 */
+    e.preventDefault();
+    if (current.isFocused()) {
+      current.setValue();
     }
-  }
-  else if (e.keyCode == '37') {
-    // left arrow
-    let nextCol = currentCol - 1;
-    let nextCell = cells.find((a) => a.row == currentRow && a.column == nextCol);
-    if(nextCell){
-      setCurrent(nextCell)
-    }
-  }
-  else if (e.keyCode == '39') {
-    // right arrow
-    let nextCol = currentCol + 1;
-    let nextCell = cells.find((a) => a.row == currentRow && a.column == nextCol);
-    if(nextCell){
-      setCurrent(nextCell)
+    current.toggleFocus();
+    return;
+  } else {
+    if (!current.isFocused()) {
+      current.toggleFocus();
     }
   }
 }
 
-function setCurrent(nextCell)
-{
-  current.getDomReference().classList.remove("current-cell");
-  current.getDomReference().blur()
+function setCurrent(nextCell) {
+  current.getDomReference().style.backgroundColor = "rgba(255, 255, 255, 0.8)";
   current = nextCell;
-  current.getDomReference().focus()
-  current.getDomReference().classList.add("current-cell");
+  current.getDomReference().style.backgroundColor = "red";
 }
